0da6c01da20560a58c38303130ab8ad64940ce1d
Elizabeth <elizabethtreahy@gmail.com>
2022-03-17 12:46:07 -0400

    commit tree 29d64e8974c570556a0164e7b7f569082219c82a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, thingImLookingFor) {
+  return drivers.filter((x) => x.toLowerCase() === thingImLookingFor.toLowerCase());
+}
+
+function fuzzyMatch(drivers, otherThingImLookingFor) {
+  return drivers.filter((x) => x.toLowerCase().indexOf(otherThingImLookingFor.toLowerCase()) === 0);
+}
+
+function matchName(drivers, lastThingImLookingFor) {
+  return drivers.filter((x) => x.name === lastThingImLookingFor);
+}
